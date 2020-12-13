62ad2ecb600d9f72f3e81629df1ee659e38091f6
iwandepe <iwan.dprakoso@gmail.com>
2020-12-13 15:29:24 +0700

    membuat class SpaceShip, Missile, dan Alien

---
+++ Collision-Detection/CollisionDetection-sc/src/id/ac/its/kelompokxyz/Alien.java
@@ -0,0 +1,28 @@
+package id.ac.its.kelompokxyz;
+
+public class Alien extends Sprite {
+
+    private final int INITIAL_X = 1280;
+    private final int ALIEN_SPEED = 5;
+
+    public Alien(int x, int y) {
+        super(x+800, y*2+50);
+
+        initAlien();
+    }
+
+    private void initAlien() {
+
+        loadImage("resources/alien.png");
+        getImageDimensions();
+    }
+
+    public void move() {
+
+        if (x < 0) {
+            x = INITIAL_X;
+        }
+
+        x -= 1 * ALIEN_SPEED;
+    }
+}
