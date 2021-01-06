b7e6e1bc377ce33be024c30753234738230091fa
botfade <erikfaderik@gmail.com>
2021-01-07 00:24:57 +0700

    modify panel class structure

--- FinalProject/src/id/ac/its/kelompokxyz/view/MenuPanel.java
+++ FinalProject/src/id/ac/its/kelompokxyz/view/MenuPanel.java
@@ -1,52 +1,42 @@
 package id.ac.its.kelompokxyz.view;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.event.MouseEvent;
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import id.ac.its.kelompokxyz.app.Commons;
-import id.ac.its.kelompokxyz.app.CreateIO;
-import id.ac.its.kelompokxyz.app.Credits;
-import id.ac.its.kelompokxyz.app.DifficultyPanel;
-import id.ac.its.kelompokxyz.app.MyButton;
-import id.ac.its.kelompokxyz.app.ReadIO;
-import id.ac.its.kelompokxyz.view.ViewButtonListener;
 
 public class MenuPanel extends JPanel{
 
 	private static final long serialVersionUID = 1L;
-	JPanel menuPanel = new JPanel();
-	JFrame frame;
 	JLabel hScore = new JLabel("BEST SCORE : 0");
 	JLabel coin = new JLabel("Coins : 0");
 
-	public MenuPanel(JFrame frame, ViewButtonListener viewButtonListener) {
-		this.frame = frame;
+	public MenuPanel( ViewButtonListener viewButtonListener) {
 		setFocusable(true);
         requestFocusInWindow();
-        
-		menuPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-		menuPanel.setBackground(new java.awt.Color(93, 100, 111));
-		menuPanel.setLayout(new GridBagLayout());
-		menuPanel.setPreferredSize(new Dimension(Commons.WIDTH, Commons.HEIGHT));
-		menuPanel.setFocusable(true);
-		menuPanel.requestFocusInWindow();
+        setBorder(new EmptyBorder(10, 10, 10, 10));
+        setBackground(new Color(93, 100, 111));
+		setLayout(new GridBagLayout());
+		setPreferredSize(new Dimension(Commons.WIDTH, Commons.HEIGHT));
+		setFocusable(true);
+		requestFocusInWindow();
 
         GridBagConstraints gbcTitle = new GridBagConstraints();
         gbcTitle.gridwidth = GridBagConstraints.REMAINDER;
         gbcTitle.anchor = GridBagConstraints.NORTH;
         
         ImageIcon imgTitle = new ImageIcon("src/resources/title.png");
-        menuPanel.add(new JLabel(imgTitle),gbcTitle);
-        menuPanel.repaint();
+        add(new JLabel(imgTitle),gbcTitle);
+        repaint();
 
-        hScore.setFont(new java.awt.Font("Fixedsys Regular", 1, 15));
-        menuPanel.add(hScore, gbcTitle);
+        hScore.setFont(new Font("Fixedsys Regular", 1, 15));
+        add(hScore, gbcTitle);
 //        updateScore();
         
         GridBagConstraints gbcBtn = new GridBagConstraints();
@@ -62,18 +52,9 @@ public class MenuPanel extends JPanel{
         buttons.add(viewButtonListener.getbExit(), gbcBtn);
 
         gbcBtn.weighty = 1;
-        menuPanel.add(buttons, gbcBtn);
-        
-        add(menuPanel);
+        add(buttons, gbcBtn);
+
         System.out.println("Here!");
-	}
-	
-	public static void changePanel(JFrame frame, JPanel added) {
-		frame.setContentPane(added);
-		frame.invalidate();
-		frame.validate();
-		added.setFocusable(true);
-		added.requestFocusInWindow();
 	}
 	
 //	public void updateScore() {
